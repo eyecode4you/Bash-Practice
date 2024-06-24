8a259d6f53f03cf901164ef8c4d972b441c3c533
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-24 23:14:04 +0100

    Update creating_files_and_dirs.sh

--- Creating Files & Directories/creating_files_and_dirs.sh
+++ Creating Files & Directories/creating_files_and_dirs.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+touch file.txt file1.txt file2.txt file3.txt
+ls -l file.txt
+file file.txt # show file type
+rm -i file.txt
+
+# copying and moving files
+cp file3.txt file.txt
+mv file.txt file4.txt
+ls file?.txt
+
+# directories
+mkdir Test
+mv file?.txt Test/
+ls Test
+
+rm -r Test
+
+mkdir Dir
+touch Dir/file1.txt Dir/file2.txt
+ls Dir
+mv Dir newDir
+ls newDir
+
+rm -r newDir
