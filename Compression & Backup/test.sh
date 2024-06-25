f47e5f19e20c1a34f8c8ca761307eb7092053997
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-25 17:11:56 +0100

    Update test.sh

--- Compression & Backup/test.sh
+++ Compression & Backup/test.sh
@@ -4,3 +4,10 @@ cp /var/log/wtmp file2
 cp /var/log/wtmp file3
 
 ls -hs file?
+
+# compression cmds, xz employs the most compression
+gzip file1 # .gz
+bzip2 file2 # .bz2
+xz file3 #.xz
+
+ls -hs file?.*
