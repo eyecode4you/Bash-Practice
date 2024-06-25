14ed6c8cb12e4e9f4f8ceb79fe2dc4e4b0784ae9
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-25 17:32:15 +0100

    Update viewing_compressed_files.sh

--- Compression & Backup/viewing_compressed_files.sh
+++ Compression & Backup/viewing_compressed_files.sh
@@ -13,3 +13,7 @@ ls boot.log.*
 zcat boot.log.gz # zcat for .gz files
 bzcat boot.log.bz2 # bzcat for .bz2 files
 xzcat boot.log.xz # xzcat for xz files
+
+# finding out which compression method is used when extension is unknown
+mv boot.log.xz boot.log.unknownCompression
+file boot.log.unknownCompression # will show us that it is XZ compressed data
