855c4b15a5f7c4b543ce586cc1edd94c9dbc9421
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-25 18:04:04 +0100

    Update backing_up_with_tar.sh

--- Compression & Backup/backing_up_with_tar.sh
+++ Compression & Backup/backing_up_with_tar.sh
@@ -6,3 +6,9 @@ cp /var/log/wtmp file4
 
 tar -cvf archive.tar file? # archive copies of all files with tar
 tar -tf archive.tar # list archive contents
+
+rm file?
+tar -xf archive.tar # extract tar archive contents
+
+tar -Jcf archive.tar.xz file? # create xz tarball
+tar -Jxf archive.tar.xz # extract tarball contents
