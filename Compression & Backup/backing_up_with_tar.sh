1f2a903035595ed4c4bac15c0723aecf7571b152
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-25 17:58:14 +0100

    Update backing_up_with_tar.sh

--- Compression & Backup/backing_up_with_tar.sh
+++ Compression & Backup/backing_up_with_tar.sh
@@ -4,4 +4,5 @@ cp /var/log/wtmp file2
 cp /var/log/wtmp file3
 cp /var/log/wtmp file4
 
-tar -cvf archive.tar file?
+tar -cvf archive.tar file? # archive copies of all files with tar
+tar -tf archive.tar # list archive contents
