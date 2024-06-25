e2acb6f3c39d3deb3351db60bc8abac9c1a6833c
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-25 17:16:22 +0100

    Update compression.sh

--- Compression & Backup/compression.sh
+++ Compression & Backup/compression.sh
@@ -3,6 +3,7 @@ cp /var/log/wtmp file1
 cp /var/log/wtmp file2
 cp /var/log/wtmp file3
 
+# list files with human-readable sizes
 ls -hs file?
 
 # compression cmds, xz employs the most compression
@@ -11,3 +12,8 @@ bzip2 file2 # .bz2
 xz file3 #.xz
 
 ls -hs file?.*
+
+# Uncompressing files
+gunzip file1
+bunzip2 file2
+unxz file3
