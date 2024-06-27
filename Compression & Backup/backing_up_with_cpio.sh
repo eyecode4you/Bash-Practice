f6d19ac0e55dd57642ff87b1c23347d13421390d
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-28 00:32:46 +0100

    Update backing_up_with_cpio.sh

--- Compression & Backup/backing_up_with_cpio.sh
+++ Compression & Backup/backing_up_with_cpio.sh
@@ -3,3 +3,9 @@ cp /var/log/wtmp file1
 cp /var/log/wtmp file2
 cp /var/log/wtmp file3
 cp /var/log/wtmp file4
+
+# creating archive with cpio
+ls file? | cpio -o > archive.cpio
+
+# reading files within cpio archive
+cpio -itvI archive.cpio
