4dd18a6db3d7f3ac88fa0987e9456c9b1360462e
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-29 05:04:40 +0100

    Create changing_permissions.sh

---
+++ Permissions/changing_permissions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+touch file.txt
+
+# using symbolic mode
+chmod u+x file.txt # add ex perm for owner
+chmod o-r file.txt # remove read perm. for other
+chmod a=rw file.txt # set r & w for other
+chmod u+r,g-x file.txt # add r for owner, remove ex for group
