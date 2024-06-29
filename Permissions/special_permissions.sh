fa6a7d60dc0bee7e81948a12b587ca1404934b99
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-29 05:29:25 +0100

    Update special_permissions.sh

--- Permissions/special_permissions.sh
+++ Permissions/special_permissions.sh
@@ -4,3 +4,15 @@ ls -l file.txt
 mkdir dir
 ls -ld dir
 
+# SYMBOLIC MODE
+chmod u+xs file.txt
+ls -l file.txt
+chmod u-sx file.txt
+
+chmod g+sx file.txt
+ls -l file.txt
+
+chmod o+t dir
+ls -ld dir
+
+# OCTAL MODE
