827ddbcecac4b69868f7537b2f4a5540266bd1f9
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-29 05:32:08 +0100

    Update special_permissions.sh

--- Permissions/special_permissions.sh
+++ Permissions/special_permissions.sh
@@ -5,14 +5,14 @@ mkdir dir
 ls -ld dir
 
 # SYMBOLIC MODE (s for SUID & SGID, t for Sticky Bit) (must have x perm. to set special perms.)
-chmod u+xs file.txt
+chmod u+xs file.txt # run file with owner perms.
 ls -l file.txt
 chmod u-sx file.txt
 
-chmod g+sx file.txt
+chmod g+sx file.txt # run file with group perms.
 ls -l file.txt
 
-chmod o+t dir
+chmod o+t dir # allow sharing files in dir, only owner can delete files
 ls -ld dir
 
 # OCTAL MODE
