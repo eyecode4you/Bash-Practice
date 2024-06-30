3b7618df2ddbf6ab285b72d952f0fc40d3b8575f
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-30 03:00:55 +0100

    Update soft_links.sh

--- Hard Links & Soft Links/soft_links.sh
+++ Hard Links & Soft Links/soft_links.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+touch file.txt
+
+# creating a soft link
+ln -s file.txt fileSoftLink
+
+ls -l file* # soft links are denoted with l as file type code, no data shared with linked file
+
+rm file*
