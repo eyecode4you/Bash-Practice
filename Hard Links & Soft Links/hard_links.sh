01e97e66ce29c2a1426ca5910716c9f0d59808ab
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-30 03:08:31 +0100

    Update hard_links.sh

--- Hard Links & Soft Links/hard_links.sh
+++ Hard Links & Soft Links/hard_links.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 touch file.txt
 echo "Why Hello There" >> file.txt
+
+# create a hard link to file
+ln file.txt fileHardLink.txt
+
+# view hard link info
+ls -i file* # inode numbers will match
+
+rm file*
