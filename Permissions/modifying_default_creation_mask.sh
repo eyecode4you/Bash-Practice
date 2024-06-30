f5c8c4edbe8bf2ec4bc7de6e11637b9975e91491
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-06-30 02:51:52 +0100

    Update modifying_default_creation_mask.sh

--- Permissions/modifying_default_creation_mask.sh
+++ Permissions/modifying_default_creation_mask.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 touch file.txt
 ls -l file.txt
+
+# view default mask with umask cmd
+umask
+
+# umask octal code subtracts from default permissions (0666 or rw-rw-rw- for files) (0777 or rwxrwxrwx for dirs)
+# modify umask
+umask 0077 # e.g. to remove rwx from group and other
+umask
+
+touch file2.txt
+ls -l file2.txt # group and other will be ---
+
+rm file.txt
+rm file2.txt
