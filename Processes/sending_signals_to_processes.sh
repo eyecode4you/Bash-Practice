c683c2715dba5542c120195153d05010c7a95eae
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-01 03:10:34 +0100

    Update sending_signals_to_processes.sh

--- Processes/sending_signals_to_processes.sh
+++ Processes/sending_signals_to_processes.sh
@@ -5,5 +5,10 @@ sleep 6000& # e.g. PID = 2011
 sleep 9000& # e.g. PID = 2012
 
 kill -s SIGKILL 2010 # 9, KILL, or SIGKILL will terminate PID 2010
-
 kill -9 2011 # same as above for PID 2011
+
+kill 2012 # default kill, sends -15, TERM, SIGTERM
+
+kill %3 # kill a background job by using % and job ID
+
+killall sleep # kill all sleep processes
