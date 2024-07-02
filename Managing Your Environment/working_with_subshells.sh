89d202939559a77f423be25e012ef1660e208520
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-02 06:18:02 +0100

    Update working_with_subshells.sh

--- Managing Your Environment/working_with_subshells.sh
+++ Managing Your Environment/working_with_subshells.sh
@@ -13,3 +13,13 @@ bash # prompt appearance change will carry over
 echo $PS1
 echo $SHLVL
 exit
+
+# can also use export before env var definition:
+export PS1="New New Prompt: "
+bash
+exit
+
+# can also add export after definition with ;
+PS1="New New New Prompt: "; export PS1
+bash
+exit
