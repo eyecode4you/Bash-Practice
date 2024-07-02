6479109697d5e834624403418ea3a0d6c358219f
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-02 05:53:54 +0100

    Create path_env_variable.sh

---
+++ Managing Your Environment/path_env_variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo $PATH # shows a series of dirs where system will check to run cmds
+
+which echo
+cp /bin/echo myecho
+myecho "This won't work" # as our home dir isn't part of the $PATH var, copying and running a cmd from here won't work
+# we could run this by providing absolute ref. to new cmd /home/USERNAME/myecho "This will now work"
+
+# to add a dir to $PATH
+# PATH = $PATH:[Path of directory to add]
+# e.g. adding home dir to path (not for practical use): PATH = $PATH:/home/USERNAME
+# adding to PATH this way is non-persistant, must use environment file
