5d6e3e5b3271f0ca05f14cfa55eff4eaf5fa86a4
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-04 08:48:34 +0100

    Update mass_storage_device_info.sh

--- Working with Storage Devices/mass_storage_device_info.sh
+++ Working with Storage Devices/mass_storage_device_info.sh
@@ -3,3 +3,8 @@ lsdev # shows low-level device info. May not be installed by default. Gets info 
 ls /proc/dma # Direct Memory Access
 ls /proc/interrupts # Allows CPU to know which devices have data to send to it by their IRQs
 ls /proc/ioports # The assigned IO ports for comms between CPU and devices
+
+ls /dev/disk/by-id # Connected storage devices listed by world-wide id (WWID)
+ls /dev/disk/by-label # By label, if used
+ls /dev/disk/by-uuid # By Universal Unique ID (UUID)
+ls /dev/disk/by-path # By how a device connects to the PCI BUS
