b9c5e9ff8343840d6e3820c9cb8843c9385704bb
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-04 07:45:11 +0100

    Update storage_devices.sh

--- Working with Storage Devices/storage_devices.sh
+++ Working with Storage Devices/storage_devices.sh
@@ -4,3 +4,5 @@ lsblk -f # shows filesystem info for storage devices
 
 cat /proc/partitions # contains sys info related to disks & partitions
 cat /proc/filesystems # shows current filesystems in use by devices
+
+free -h # shows RAM & SWAP info
