2bffc30a5c932773fe529759abee57e142f43c46
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-04 07:38:37 +0100

    Update storage_devices.sh

--- Working with Storage Devices/storage_devices.sh
+++ Working with Storage Devices/storage_devices.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 lsblk # shows storage disks & partitions (lvm indicates a logical volume or grouped partitions)
+lsblk -f # shows filesystem info for storage devices
+
+cat /proc/partitions # contains sys info related to disks & partitions
+cat /proc/filesystems # shows current filesystems in use by devices
