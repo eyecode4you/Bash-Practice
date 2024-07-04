570b8116e58cd8f8101546f176d0e5744ce8d887
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-04 08:42:08 +0100

    Update mass_storage_device_info.sh

--- Working with Storage Devices/mass_storage_device_info.sh
+++ Working with Storage Devices/mass_storage_device_info.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-lsdev # shows low-level device info. Gets info from:
+lsdev # shows low-level device info. May not be installed by default. Gets info from:
 ls /proc/dma # Direct Memory Access
 ls /proc/interrupts # Allows CPU to know which devices have data to send to it by their IRQs
 ls /proc/ioports # The assigned IO ports for comms between CPU and devices
