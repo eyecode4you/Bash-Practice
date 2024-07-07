f024c570e85294e24f7d2454235f7a5b5a458bdc
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-07 10:24:38 +0100

    Update lab_8.sh

--- Compression & Backup/lab_8.sh
+++ Compression & Backup/lab_8.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+# copy some files to work with
+cp /var/log/wtmp file1
+cp /var/log/wtmp file2
+cp /var/log/wtmp file3
+
+# compressing with gzip, bzip2, and xs
+gzip file1
+bzip2 file2
+xz file3
+
+# note the compressed file sizes
+ls -lh file?.*
+
+# view gzip, bzip2, and xz files
+zcat file1.gz
+bzcat file2.bz2
+xzcat file3.xz
+
+# decompress gzip, bzip2, and xz files
+gunzip file1.gz
+bunzip2 file2.bz2
+unxz file3.xz
