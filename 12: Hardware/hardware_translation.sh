95774e80ef33f1b7cebc5751b0d022db9645c20a
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-08 10:56:36 +0100

    Update hardware_translation.sh

--- 12: Hardware/hardware_translation.sh
+++ 12: Hardware/hardware_translation.sh
@@ -2,3 +2,8 @@
 # device drivers act as translators between devices so they can interoperate
 # drivers can be found in (will change depending on distro!):
 ls /lib/modules/5.4.0-65-generic/kernel/drivers
+
+# lsmod utility shows all current kernel modules
+lsmod
+lsmod | grep joydev # search for specific module
+modinfo joydev # get more info on module
