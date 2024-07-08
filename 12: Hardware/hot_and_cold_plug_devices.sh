eb916bc081ca98069027cc9eb37f935ae2363db5
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-08 11:20:36 +0100

    Update hot_and_cold_plug_devices.sh

--- 12: Hardware/hot_and_cold_plug_devices.sh
+++ 12: Hardware/hot_and_cold_plug_devices.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# udev dets up cold plug devices, to view info requires admin
+# udev sets up cold plug devices, to view info requires admin
 su -c "udevadm info /dev/sda1 | less"
+
+# view hot plug USB device:
+su -c "udevadm info /dev/sdb1 | less"
+ls -d /media # hot plug devices are auto-mounted here
+
+# udev will check here if no config rules are provided for a device:
+ls /etc/udev/rules.d
