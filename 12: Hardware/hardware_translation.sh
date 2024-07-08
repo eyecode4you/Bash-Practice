b338a96426c124a1ed63fb1bb019959ccf62abe2
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-08 11:07:16 +0100

    Update hardware_translation.sh

--- 12: Hardware/hardware_translation.sh
+++ 12: Hardware/hardware_translation.sh
@@ -6,4 +6,15 @@ ls /lib/modules/5.4.0-65-generic/kernel/drivers
 # lsmod utility shows all current kernel modules
 lsmod
 lsmod | grep joydev # search for specific module
-modinfo joydev # get more info on module
+modinfo joydev # get more info on module, also shows absolute path 
+
+# removing a kernel module:
+sudo rmmod joydev # rmmod removes single module but not its dependents
+
+# inserting a kernal module, requires absolute dir:
+sudo insmod /lib/modules/5.4.0-65-generic/kernel/drivers/input/joydev.ko # inserts single module but not its dependents
+
+# modprobe is a kernel management utility, better than insmod and rmmod
+sudo modprobe -v dm-mirror # will insert dm-mirror and all its dependencies, doesn't require absolute path
+lsmod | grep dm_mirror
+sudo modprobe -rv dm-mirror # will remove dm-mirror and all its dependencies
