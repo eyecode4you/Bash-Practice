75c1612216b7deb1fd22bc291ea5cfb1b3b75a04
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-08 10:52:52 +0100

    Update hardware_translation.sh

--- 12: Hardware/hardware_translation.sh
+++ 12: Hardware/hardware_translation.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+# device drivers act as translators between devices so they can interoperate
+# drivers can be found in (will change depending on distro!):
+ls /lib/modules/5.4.0-65-generic/kernel/drivers
