c644138e61eecf003d33840f4c1693f27b6375fb
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-09 11:00:36 +0100

    Update making_filesystems.sh

--- 13: Managing Storage/making_filesystems.sh
+++ 13: Managing Storage/making_filesystems.sh
@@ -7,5 +7,8 @@ grep XFS /proc/filesystems # check if a specific filesystem is supporte
 ls /lib/modules/*/kernel/fs
 
 # e.g. make an ext4 fs for sdb1
-sudo mkfs -t ext4 /dev/sdb1
-sudo mkfs.ext4 /dev/sdc2 # can also use . method
+# sudo mkfs -t ext4 /dev/sdb1
+# sudo mkfs.ext4 /dev/sdc2 # can also use . method
+
+# e.g. maing a swap partition:
+# sudo mkswap /dev/sdc3
