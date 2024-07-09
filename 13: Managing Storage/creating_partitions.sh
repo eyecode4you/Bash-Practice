736254db019b6dbc3400d938dbdbecf1237d2dd1
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-09 10:18:52 +0100

    Update creating_partitions.sh

--- 13: Managing Storage/creating_partitions.sh
+++ 13: Managing Storage/creating_partitions.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 cat /proc/partitions # view system partition info
+lsblk | grep -E "sd[abc]?" # view disks & partitions etc.
+
+# partitions can be created with the gdisk utility (requires admin)
+# e.g. sudo gdisk /dev/sdb
+# sudo gdisk -l /dev/sdb # can be used to list partition info
+
+# another utility is fdisk
+# e.g. sudo fdisk /dev/sdc
+# sudo fdisk -l /dev/sdc
+
+# another is the parted utility
+# e.g. sudo parted /dev/sdc
