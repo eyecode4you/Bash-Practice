f21ab9ebc72f8f5c925ea02be5188976d7f29416
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-09 11:30:25 +0100

    Update mounting_filesystems.sh

--- 13: Managing Storage/mounting_filesystems.sh
+++ 13: Managing Storage/mounting_filesystems.sh
@@ -3,8 +3,8 @@ lsblk -f # view partitions & filesystems
 sudo blkid # another way of doing this
 
 # manually mount a filesystem
- mkdir temp
- sudo mount -t ext4 /dev/sdb1 temp # can also use a UUID instead of path with -U
+mkdir temp
+sudo mount -t ext4 /dev/sdb1 temp # can also use a UUID instead of path with -U
 
 # view all mounted fs
 mount
@@ -15,3 +15,11 @@ sudo umount temp # can also use /dev/sdb1 instead of temp
 
 # auto-mounting fs
 cat /etc/fstab # config used by OS for auto-mounting
+sudo cp /etc/fstab /etc/fstab.bck # create a backup of fstab
+sudo mkdir /newMount # make a new dir to mount to
+sudo nano /etc/fstab # edit config
+# e.g. adding: /dev/sdc2 /newMount ext4 defaults 0 0
+
+sudo mount -a # will auto mount all unmounted fs in fstab
+mount | grep newMount # check
+# /dev/sdc2 will now auto-mount to newMount on boot
