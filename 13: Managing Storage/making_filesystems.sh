0f95fe9cd879e3c14810cc082a2a63954e8b88bd
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-09 10:54:47 +0100

    Update making_filesystems.sh

--- 13: Managing Storage/making_filesystems.sh
+++ 13: Managing Storage/making_filesystems.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+lsblk -f | grep -E "sd[abc]?" # list partitions & filesystems in use
+ls /proc/filesystems # see which filesystems are supported
+grep XFS /proc/filesystems # check if a specific filesystem is supporte
+
+# if not, we can check if there's any kernel module for it:
+ls /lib/modules/*/kernel/fs
