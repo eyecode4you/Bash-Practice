25ddfc254123334445f9e6a31647d2178cbbae3c
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-09 10:58:38 +0100

    Update making_filesystems.sh

--- 13: Managing Storage/making_filesystems.sh
+++ 13: Managing Storage/making_filesystems.sh
@@ -5,3 +5,7 @@ grep XFS /proc/filesystems # check if a specific filesystem is supporte
 
 # if not, we can check if there's any kernel module for it:
 ls /lib/modules/*/kernel/fs
+
+# e.g. make an ext4 fs for sdb1
+sudo mkfs -t ext4 /dev/sdb1
+sudo mkfs.ext4 /dev/sdc2 # can also use . method
