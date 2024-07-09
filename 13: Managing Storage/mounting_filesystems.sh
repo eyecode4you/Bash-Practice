ec9835295a502e0f08d64ab5975e9270f428da97
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-09 11:11:17 +0100

    Update mounting_filesystems.sh

--- 13: Managing Storage/mounting_filesystems.sh
+++ 13: Managing Storage/mounting_filesystems.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+lsblk -f # view partitions & filesystems
+sudo blkid # another way of doing this
+
+# mount a filesystem
+ mkdir temp
+ sudo mount -t ext4 /dev/sdb1 temp # can also use a UUID instead of path with -U
+
+# view all mounted fs
+mount
+mount | grep temp # search for a specific mount
+
+# unmount fs
+sudo umount temp # can also use /dev/sdb1 instead of temp
