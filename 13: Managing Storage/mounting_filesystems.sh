f766c59fb53b85d3e0a39da7f826a3641bbd5960
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-09 11:16:25 +0100

    Update mounting_filesystems.sh

--- 13: Managing Storage/mounting_filesystems.sh
+++ 13: Managing Storage/mounting_filesystems.sh
@@ -2,7 +2,7 @@
 lsblk -f # view partitions & filesystems
 sudo blkid # another way of doing this
 
-# mount a filesystem
+# manually mount a filesystem
  mkdir temp
  sudo mount -t ext4 /dev/sdb1 temp # can also use a UUID instead of path with -U
 
@@ -12,3 +12,6 @@ mount | grep temp # search for a specific mount
 
 # unmount fs
 sudo umount temp # can also use /dev/sdb1 instead of temp
+
+# auto-mounting fs
+cat /etc/fstab # config used by OS for auto-mounting
