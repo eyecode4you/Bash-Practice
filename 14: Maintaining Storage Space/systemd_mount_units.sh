042e8333b2211f94370957a8d57d4444d553f118
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-10 12:46:02 +0100

    Update systemd_mount_units.sh

--- 14: Maintaining Storage Space/systemd_mount_units.sh
+++ 14: Maintaining Storage Space/systemd_mount_units.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+cat /etc/fstab # determines what filesystems systemd will mount and where when system boots
+systemctl -t mount list-units # view systemd mount units in use
+ls /etc/systemd/system/*.mount # check for custom mount unit files
