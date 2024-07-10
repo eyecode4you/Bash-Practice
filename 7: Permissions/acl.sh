b8767b423dc39e78ac27bacd24b75828d67b2993
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-10 16:18:58 +0100

    Update acl.sh

--- 7: Permissions/acl.sh
+++ 7: Permissions/acl.sh
@@ -2,8 +2,21 @@
 # ACLs allow you to define fine-grained discretionary access for files & dirs
 touch confidential.txt
 
-# Add permission for single user (e.g. david)
+# Add permission for single user (e.g. david):
 setfacl -m u:david:rwx confidential.txt
 
-# add permissions for single group (e.g. disco)
+# add permissions for single group (e.g. disco):
 setfacl -m g:disco:rw confidential.txt
+
+# to allow all files/dirs within a dir to inherit ACL:
+setfacl -Rm u:david:rwx /home/user
+
+# to remove a specific ACL entry:
+setfacl -x u:david confidential.txt
+setfacl -x g:disco confidential.txt
+
+# to remove all ACL entries on a file/dir:
+setfacl -b /home/user
+
+# ACL entries add a + to long listings
+# Setting w perm with ACL does not allow deletion of the file/dir
