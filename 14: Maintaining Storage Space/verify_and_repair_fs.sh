37b2bcad02cd561ca87e2bba474c8d491198675c
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-10 13:13:36 +0100

    Update verify_and_repair_fs.sh

--- 14: Maintaining Storage Space/verify_and_repair_fs.sh
+++ 14: Maintaining Storage Space/verify_and_repair_fs.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+# check ext fs integrity with fsck (file system check) cmd. Ensure target is unmounted!
+mount | grep sdc2 # check if target fs is mounted
+sudo fsck -r /dev/sdc2 # status 0 means no errors, -r provides report, -c progress bar
+
+# can also use e2fsck
+sudo e2fsck /dev/sdc2
+
+# for XFS
+# xfs_repair -> check and repair
+# xfs_db or xfs_repair -n -> only check
+
+# for btrfs
+# btrfs check -> check + repair
+# btrfsck (deprecated) -> check + repair
