76948d9ea2b66b50f0a04063d292f36c1db4f28f
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-11 12:07:50 +0100

    Update GRUB2_boot.sh

--- 15: Booting the Linux System/GRUB2_boot.sh
+++ 15: Booting the Linux System/GRUB2_boot.sh
@@ -10,3 +10,14 @@ ls /boot/grub2/grub.cfg
 
 # for UEFI systems, GRUB2 cfg is in:
 ls /boot/efi/EFI/*/grub.cfg
+
+head -20 /boot/grub/grub.cfg
+# We don't edit grub.cfg directly, settings are changed in:
+    # /etc/default/grub
+    # or by creating template with /etc/grub.d
+
+cat /etc/default/grub
+# TIMEOUT=0 or TIMEOUT_STYLE=hidden means no boot menu is displayed
+
+ls /etc/grub.d/
+# for custom entries to GRUB2 boot menu, modify either 40_custom or 41_custom
