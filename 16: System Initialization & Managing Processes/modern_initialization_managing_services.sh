11a59ea194059bd7b02794e00b9582288698a5d2
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-13 14:09:48 +0100

    Update modern_initialization_managing_services.sh

--- 16: System Initialization & Managing Processes/modern_initialization_managing_services.sh
+++ 16: System Initialization & Managing Processes/modern_initialization_managing_services.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 # determine a service's status
+systemctl status network-manager
 
 # starting/stopping services
+sudo systemctl stop network-manager
+systemctl status network-manager
+sudo systemctl start network-manager
+systemctl status network-manager
 
 # setting services to start/stop at boot
+# Loaded: line shows whether enabled/disabled
+sudo systemctl disable network-manager # stop at boot
+systemctl status network-manager
+sudo systemctl enable network-manager # start at boot
+systemctl status network-manager
 
 # jumping service targets
+systemctl get-default
+sudo systemctl isolate multi-user.target
+sudo systemctl isolate runlevel1.target # can also use runlevels
