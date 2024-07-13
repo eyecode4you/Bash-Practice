cfd93c733dcf82db500e748e61745dc424df3983
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-13 13:24:01 +0100

    Update classic_initialization.sh

--- 16: System Initialization & Managing Processes/classic_initialization.sh
+++ 16: System Initialization & Managing Processes/classic_initialization.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 ls -d /etc/rc.d/rc?.d
+# will show dirs rc0.d, rc1.d, etc. used for the different runlevels
+# In these dirs are the services that are started (S) or killed (K) in numbered order
+# Services listed in these dirs are symbolic links
+ls -l /etc/rc.d/rc3.d/*syslog
+readlink -f /etc/rc.d/rc3.d/*syslog # /etc/rc.d/init.d/syslog
+
+# /etc/rc.d/init.d containes the scripts used in runlevel dirs
+# To modify a script, instead of going into each runlevel dir, change it here
+ls /etc/rc.d/init.d
