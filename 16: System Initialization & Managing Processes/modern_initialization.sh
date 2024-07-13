5b407b4a7534f9b2f0bfcee5c80764e217551b55
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-13 13:45:44 +0100

    Update modern_initialization.sh

--- 16: System Initialization & Managing Processes/modern_initialization.sh
+++ 16: System Initialization & Managing Processes/modern_initialization.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+# target is a group of services to be started
+
+# systemd + services started at boot
+systemctl get-default
+
+# systemd unit & target files
+sudo systemctl set-default multi-user.target
+systemctl get-default
+sudo systemctl set-default graphical.target
+
+# view/modify systemd config
+systemctl list-unit-files
+systemctl list-unit-files | grep ^syslog
+# Service statuses:
+  # Enabled: Service starts at boot
+  # Disabled: Service doesn't start at boot
+  # Static: Service only starts if manually started or another service needs it at boot
+
+systemctl cat syslog.service # shows info about service and its config
+
+# systemd unit file locations
+ls /etc/systemd/system
+ls /run/systemd
+ls /usr/lib/systemd
