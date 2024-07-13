c3454a219c8ba7a919e7422867794a9b43b183f7
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-13 19:34:47 +0100

    Create dpkg.sh

---
+++ 17: Managing Debian Software/dpkg.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Package structure: 
+# [name]_[version]-[release]_[arch].deb e.g. gimp_2.8.22-1_amd64.deb
+
+# list package contents (--contents)
+dpkg -c gimp_2.8.22-1_amd64.deb
+
+# view package info (--info)
+dpkg -I gimp_2.8.22-1_amd64.deb | less
+
+# check if package is installed (--status), only need package name
+dpkg -s gimp | less
+
+# install & update package (--install)
+sudo dpkg -i gimp_2.8.22-1_amd64.deb
+dpkg -s gimp | less
+
+# verify package files (--verify)
+sudo dpkg -V gimp # no output means verified
+
+# check for broken package
+sudo dpkg -C gimp # no output means ok
+
+# remove a package
+sudo dpkg -r gimp # doesn't remove dependencies
+
+# reconfigure an installed package
+# sudo dpkg-reconfigure apparmor
