f6e13db4055ffa69024f8bd2832556ac4153d67a
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-13 19:43:17 +0100

    Update apt.sh

--- 17: Managing Debian Software/apt.sh
+++ 17: Managing Debian Software/apt.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+# config file that contains repos for sys:
+cat /etc/apt/sources.list
+
+# update repo info
+sudo apt-get update
+
+# install or update a package, e.g. procinfo
+sudo apt-get install procinfo
+
+# remove a package
+sudo apt-get remove procinfo
+
+# get info about specified package dependencies
+apt-cache depends procinfo
+
+# get more info about package
+apt-cache showpkg procinfo
+
+# show any unmet dependencies for installed packages
+apt-cache unmet
