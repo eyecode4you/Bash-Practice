262aaced69292636f058f304850904ab40dfdab3
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-14 21:27:23 +0100

    Update rpm.sh

--- 18: Managing Red Hat Software/rpm.sh
+++ 18: Managing Red Hat Software/rpm.sh
@@ -4,3 +4,33 @@
 ls *.rpm
 # e.g. iotop-0.6-4.el7.noarch.rpm
 
+# check if package is installed
+rpm -q firefox
+
+# query package and get info
+rpm -qi firefox # won't show dep info
+
+# query package dependencies
+rpm -qR firefox
+
+# query what package provides a specified file 
+rpm -q --whatprovides /bin/top
+
+# install a package
+rpm -i iotop-0.6-4.el7.noarch.rpm
+
+# update a package (will install if not installed)
+rpm -U iotop-0.6-4.el7.noarch.rpm
+
+# uninstall
+rpm -e iotop
+
+# verify a package
+rpm -V iotop # no response means ok
+
+# get info on package that's not installed
+rpm -p iotop-0.6-4.el7.noarch.rpm
+
+# to extract files from package without installing:
+# rpm2cpio [package file].rpm > [name].cpio
+# cpio -idv < [name].cpio
