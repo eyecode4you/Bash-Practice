7ec9ed4c26c7fd08393a56f67653ef4611373225
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-14 21:46:56 +0100

    Update zypper.sh

--- 18: Managing Red Hat Software/zypper.sh
+++ 18: Managing Red Hat Software/zypper.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+# zypper may not be available on CentOS or Ubuntu, try OpenSUSE
+cat /etc/zypp/zypper.conf # zypper conf file
+ls /etc/zypp/repos.d # zypper repos
+
+# view zypper repos used by system
+zypper lr
+
+# update (refresh) zypper repo info
+sudo zypper refresh # or zypper ref
+
+# install a package
+sudo zypper install iotop
+
+# check if package is installed
+sudo zypper search -i iotop # i+ indicates it's installed
+sudo zypper what-provides /usr/sbin/iotop
+
+# update a package
+sudo zypper update iotop
+sudo zypper update # update all packages
+
+# uninstall a package
+sudo zypper remove iotop
+
+# get info about package
+zypper info iotop
+
+# verify if installed packages have dependencies met
+sudo zypper verify
+
+# check if updates are available for packages
+sudo zypper list-updates
+
+zypper # will show info on cmd use
