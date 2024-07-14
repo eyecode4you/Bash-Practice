dd441d8f7ce60b3fb1eb420550cfa9316e123d31
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-14 21:34:01 +0100

    Update yum.sh

--- 18: Managing Red Hat Software/yum.sh
+++ 18: Managing Red Hat Software/yum.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-# yum = Yellowdog
+# yum = Yellowdog, most cmds need su
 cat /etc/yum.conf
 ls /etc/yum.repos.d
+
+# yum auto-updates repo info when issuing cmds
+# check if package is installed
+yum list iptraf-ng
+
+# install a package
+yum install iptraf-ng
+
+# cleanup yum cache
+yum clean all
+
+# update a package
+yum update iotop
+yum update # update all packages
