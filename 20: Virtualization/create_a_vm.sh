9b8d747059a5d64630239597f3c803718a9b143f
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-15 17:53:45 +0100

    Create create_a_vm.sh

---
+++ 20: Virtualization/create_a_vm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Before Creting a VM:
+# Research host system's BIOS for virtualization support
+# Check host system resources
+  # Enough processing to support guest VMs - top
+  # Available RAM - free
+  # Free disk space available - du
+  # How many NICs
+# Check if CPU extensions support virtualization - /proc/cpuinfo
