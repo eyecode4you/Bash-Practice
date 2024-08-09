19773b859d3011ea908f4021ffbcb0b8c6767df3
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-08-09 16:31:56 +0100

    Create basicScript.sh

---
+++ 21: Bash Scripting/basicScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# # are used for comments and spacing within script.
+#
+pwd
+ls -l *.sh
+#
+exit
+
+# Running Scripts:
+# bash basicScript.sh
+# ./basicScript.sh (requires x perms: e.g. chmod u+x basicScript.sh)
