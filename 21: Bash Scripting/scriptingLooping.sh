d9341cc0572fe3be3d2d6711028933cff92e047f
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-08-11 21:47:24 +0100

    Update scriptingLooping.sh

--- 21: Bash Scripting/scriptingLooping.sh
+++ 21: Bash Scripting/scriptingLooping.sh
@@ -1,1 +1,37 @@
 #!/bin/bash
+#
+# FOR LOOP:
+for loopVar in A B C D E F
+do
+    echo "Letter is: "$loopVar
+done
+echo
+#
+#
+# FOR LOOP with seq (sequence) cmd:
+for loopVar in $(seq 1 6)
+do
+    echo "Number is: "$loopVar
+done
+echo
+#
+#
+# WHILE LOOP:
+loopVar=1
+while [ $loopVar -ne 10 ]
+do
+    echo "Number is: "$loopVar
+    loopVar=$[ $loopVar + 1 ]
+done
+echo
+#
+#
+# UNTIL LOOP:
+loopVar=1
+until [ $loopVar -eq 100000 ]
+do
+    echo "Number is: "$loopVar
+    loopVar=$[ $loopVar * 10 ]
+done
+#
+exit
